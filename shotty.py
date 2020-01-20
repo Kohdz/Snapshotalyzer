6c51a73fab99a3cb47b2f4facdb10c3c09fb9974
kohdz <umair.stem@gmail.com>
2020-01-19 23:23:28 -0500

    Starting and Stopping Instances

--- shotty.py
+++ shotty.py
@@ -4,23 +4,76 @@ import click
 session = boto3.session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
+
+def filter_instances(project):
+    instances = []
+    if project:
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
 # we are using a decorator
 # we are handing our function to click
 # click gives error if we run our project in and unexpected way
 # also, if we use --help, it returns a help option
-@click_command()
-def list_instances():
+
+
+@click.group()
+def instances():
+    """ Commands for instances"""
+
+
+@instances.commands('list')
+@click.option('--project', default=None,
+              help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
     "list EC2 instances"
-    for i in ec2.instances.all():
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        tags = {t['key']: t['Value'] for t in i.tags or []}
         print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
-            i.public_dsn_name)))
+            i.public_dsn_name,
+            tags.get('Project', '<no project>'))))
+
+    return
+
+
+@instances.command('stop')
+@click.option('--project', default=None,
+              help='Only instances for project')
+def stop_instances(project):
+    "Stop EC2 instances"
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("stopping {0}..".format(i.id))
+        i.stop()
+
+    return
+
+
+@instances.command('start')
+@click.option('--project', default=None,
+              help='Only instances for project')
+def start_instances(project):
+    "Starting EC2 instances"
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("starting {0}..".format(i.id))
+        i.start()
 
     return
 
 
 if __name__ = '__main__':
-    list_instances()
+    instances()
