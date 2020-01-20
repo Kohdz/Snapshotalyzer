2a52e6f87accdddb4a2af9e910ffe86b7968cb37
kohdz <umair.stem@gmail.com>
2020-01-19 23:03:00 -0500

    Update to use click

--- shotty.py
+++ shotty.py
@@ -1,9 +1,26 @@
 import boto3
+import click
+
+session = boto3.session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+# we are using a decorator
+# we are handing our function to click
+# click gives error if we run our project in and unexpected way
+# also, if we use --help, it returns a help option
+@click_command()
+def list_instances():
+    "list EC2 instances"
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dsn_name)))
+
+    return
+
 
 if __name__ = '__main__':
-
-    session = boto3.session(profile_name='shotty')
-    ec2 = session.resource('ec2')
-
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
