2ff5e87eb49b113def6fc66bb63997cd64eba18a
kohdz <umair.stem@gmail.com>
2020-01-19 22:51:34 -0500

    list instances

---
+++ shotty.py
@@ -0,0 +1,9 @@
+import boto3
+
+if __name__ = '__main__':
+
+    session = boto3.session(profile_name='shotty')
+    ec2 = session.resource('ec2')
+
+    for i in ec2.instances.all():
+        print(i)
